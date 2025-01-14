1ae4681906403b59057611abbac6442757d6640f
saeed <mvvva001@gmail.com>
2025-01-15 01:50:48 +0500

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Countries from "./pages/Coins";
 import Header from "./components/Header";
-import { useState } from "react";
 import SingleCoin from "./pages/SingleCoin";
 import Markets from "./pages/Markets";
 import Trade from "./pages/Trade";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div>
       <Router>
-        <Header setIsOpen={setIsOpen} />
+        <Header />
         <Routes>
-          <Route
-            path='/'
-            element={<Countries isOpen={isOpen} setIsOpen={setIsOpen} />}
-          />
-          <Route path='/markets' element={<Markets />} />
-          <Route path='/trade' element={<Trade />} />
-          <Route path='/coin/:id' element={<SingleCoin />} />
+          <Route path="/" element={<Countries />} />
+          <Route path="/markets" element={<Markets />} />
+          <Route path="/trade" element={<Trade />} />
+          <Route path="/coin/:id" element={<SingleCoin />} />
         </Routes>
       </Router>
     </div>
